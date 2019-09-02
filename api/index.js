586fd0e87d8a9b40586b0d0f6f68a2b7eff48754
ladysamantha <ladysamantha@protonmail.com>
2019-09-01 22:03:31 -0600

    add file watching and bug fix

--- api/index.js
+++ api/index.js
@@ -4,15 +4,15 @@ const port = Number.parseInt(process.env.PORT) || 3000
 
 const app = express()
 
-const shutdown = () => {
-  console.log('gracefully shutting down server')
-  app.close()
-}
-
 app.use('*', (req, res) => res.send('OK'))
 
 if (require.main === module) {
-  app.listen(port, () => console.log(`server listening on port ${port}`))
+  const server = app.listen(port, () => console.log(`server listening on port ${port}`))
+
+  const shutdown = () => {
+    console.log('gracefully shutting down server')
+    server.close()
+  }
 
   process.on('SIGTERM', shutdown)
   process.on('SIGINT', shutdown)
